ece413c84a1ede10deac5463d14042c0f66f26d1
SHRALLE <rasmusnissendahl@gmail.com>
2021-05-12 11:43:18 +0200

    skubbe

--- dataset_cleanup.py
+++ dataset_cleanup.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 data = pd.read_csv("data/US_Accidents_Dec20.csv")
-CleanedData = data.drop(['Number','Distance(mi)','Airport_Code','Street','Side','Country','Amenity','Bump','Crossing','Give_Way','Junction','No_Exit','Railway','Roundabout','Station','Stop','Traffic_Calming','Traffic_Signal','Turning_Loop','Sunrise_Sunset','Civil_Twilight','Nautical_Twilight','Astronomical_Twilight', 'End_Lat','End_Lng','Precipitation(in)'],axis='columns', inplace=False)
+CleanedData = data.drop(['Number','Distance(mi)','Airport_Code','Street','Side','Country','Amenity','Bump','Crossing','Give_Way','Junction','No_Exit','Railway','Roundabout','Station','Stop','Traffic_Calming','Traffic_Signal','Turning_Loop','Sunrise_Sunset','Civil_Twilight','Nautical_Twilight','Astronomical_Twilight', 'End_Lat','End_Lng'],axis='columns', inplace=False)
 data.info()
 
 CleanedDataV2 = CleanedData.dropna()
