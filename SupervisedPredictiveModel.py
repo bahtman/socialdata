0c030f3228a7c14d191ef5ffe0786c903eacbb59
Fredferik Kjaer <f.kjaer@icloud.com>
2021-05-12 13:37:41 +0200

    Encoded weather condition plus har delt data op

--- SupervisedPredictiveModel.py
+++ SupervisedPredictiveModel.py
@@ -2,25 +2,36 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 data = pd.read_csv("data/CleanedData.csv")
+Weather_Conditions = data['Weather_Condition'].unique()
+data['Weather_Condition'] = data['Weather_Condition'].astype('category')
+data['Weather_Condition_Encoded'] = data['Weather_Condition'].cat.codes
 
-X = data.drop(labels = ['Severity'], axis = 1, inplace = False)
-y = data['Severity']
+train_df, test_df = train_test_split(data, test_size=0.2)
 
-X_scaled = X[['Visibility(mi)','Wind_Speed(mph)','Pressure(in)','Humidity(%)','Temperature(F)']]
-scaler = preprocessing.StandardScaler().fit(X_scaled)
-X_scaled = scaler.transform(X_scaled)
+#Define train X and y train set
+X_train = train_df.drop(labels = ['Severity'], axis = 1, inplace = False)
+y_train = train_df['Severity']
 
-#Forsøg 1
-clf = LogisticRegression(random_state=0).fit(X_scaled, y)
-clf.predict(X[:2, :])
-clf.predict_proba(X[:2, :])
-clf.score(X, y)
+#Define train X and y test set
+X_test = test_df.drop(labels = ['Severity'], axis = 1, inplace = False)
+y_test = test_df['Severity']
+
+#Scale train set
+X_train_scaled = X_train[['Visibility(mi)','Wind_Speed(mph)','Pressure(in)','Humidity(%)','Temperature(F)','Weather_Condition_Encoded']]
+scaler = preprocessing.StandardScaler().fit(X_train_scaled)
+X_train_scaled = scaler.transform(X_train_scaled)
+
+#Scale test set_
+X_test_scaled = X_test[['Visibility(mi)','Wind_Speed(mph)','Pressure(in)','Humidity(%)','Temperature(F)','Weather_Condition_Encoded']]
+scaler = preprocessing.StandardScaler().fit(X_test_scaled)
+X_test_scaled = scaler.transform(X_test_scaled)
 
 #Forsøg 2
 
 model = LogisticRegression(solver='lbfgs')
-model.fit(X_scaled, y)
-yhat = model.predict(X_scaled)
-acc = accuracy_score(y, yhat)
+model.fit(X_train_scaled, y_train)
+yhat = model.predict(X_test_scaled)
+acc = accuracy_score(y_test, yhat)
 print(acc)
